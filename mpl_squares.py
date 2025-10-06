df6ba7551d35dc5147ed3dd8bb7605319906be62
gonkikong0 <toshivy@outlook.com>
2025-10-06 22:25:11 +0530

    Changing labels and thickness

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,6 +3,15 @@ import matplotlib.pyplot as plt
 squares = [1,4,9,16,25]
 
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth = 10)
+
+
+#Set up Chart titles and label Axes
+ax.set_title("Square Numbers", fontsize = 24)
+ax.set_xlabel("Value", fontsize = 14)
+ax.set_ylabel("Square of the value", fontsize = 14)
+
+#Set size of tick labels
+ax.tick_params(axis= 'both', labelsize= 14)
 
 plt.show()
