cadc512455732d292d6b2e6ee47fddff6d8083ee
gonkikong0 <toshivy@outlook.com>
2025-10-09 21:57:18 +0530

    Saving PLOTS

--- scatter_squares.py
+++ scatter_squares.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 x_values = range(1,1001)
 y_values = [x**2 for x in x_values]
 
-plt.style.use('fivethirtyeight')
+# plt.style.use('fivethirtyeight')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values , s = 3) #s = markersize (x,y) [data positions]
+ax.scatter(x_values, y_values ,c = y_values, cmap=plt.cm.Blues , s = 3) #s = markersize (x,y) [data positions]
+# The color ('c') can also be passed using RGB Color Model, example : c = (0, 0.9, 0) [ranging from 0 - 1]
 
 #Set chart title and label Axes.
 
@@ -17,4 +18,8 @@ ax.set_ylabel("Square Value", fontsize = 14)
 #Set size of tick labels.
 ax.tick_params(axis='both', which = 'major', labelsize = 14)
 
-plt.show()  
+#Set the range for each axis.
+ax.axis([0, 1100, 0, 1100000]) 
+
+# plt.show() #This only shows the plots
+plt.savefig('first_plot.png', bbox_inches ='tight')
