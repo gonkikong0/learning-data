4b59569fd8c4979a71fb88993b83d08873d4f448
gonkikong0 <toshivy@outlook.com>
2025-10-12 22:51:37 +0530

    Random Walk styling

--- rw_visual.py
+++ rw_visual.py
@@ -3,11 +3,29 @@ from random_walk import RandomWalk
 
 #Making a randomwalk.
 
-rw = RandomWalk()
-rw.fill_walk()
+#Keep making new random walks, as long as the program is active.
+
+while True:
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
 
 
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap=plt.cm.Blues, edgecolors='none', s= 1)
+
+    #Emphasize the first and last points!
+    ax.scatter(0,0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Remove the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+  
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n)")
+    if keep_running == 'n':
+        break
