9ee3a608be46d8978707051d6f64c23757ae9c1a
gonkikong0 <toshivy@outlook.com>
2025-10-25 14:43:07 +0530

    Visualizing 2 D6 Dice

--- die_visual copy.py
+++ die_visual copy.py
@@ -6,29 +6,28 @@ from plotly import offline
 from die import Die
 
 #Creating a D6
-dice_1 = Die()
-dice_2 = Die()
+die = Die()
+
 #Rolling and storing results in a list.
 results = []
 for roll_num in range(1000):
-    result = dice_1.roll() + dice_2.roll()
+    result = die.roll()
     results.append(result)
 
 #ANALYZING THE RESULTS
 frequencies = []
-max_result = dice_1.num_sides + dice_2.num_sides
- 
-for value in range(2, max_result + 1):
+
+for value in range(1, die.num_sides + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 #Visualize the results -
 
-x_values = list(range(2, max_result+ 1))
+x_values = list(range(1, die.num_sides+1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_axis_config = {'title': 'Result', 'dtick': 1}
+x_axis_config = {'title': 'Result'}
 y_axis_config = {'title': 'Frequency of Result'}
 
-my_layout = Layout(title='Results of rolling two D6 1000 times', xaxis = x_axis_config, yaxis = y_axis_config)
-offline.plot({'data': data, 'layout':my_layout}, filename = 'd6_d6.html')
+my_layout = Layout(title='Results of rolling one D6 1000 times', xaxis = x_axis_config, yaxis = y_axis_config)
+offline.plot({'data': data, 'layout':my_layout}, filename = 'd6.html')
